da2c1e8508e25888806ac8baeaeeba02b5df2c8e
Oishik Chowdhury <oishik03c@gmail.com>
2023-11-08 11:13:22 +0530

    Comments

--- client/client.cpp
+++ client/client.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
 #include "definitions.h"
 #include "newclasses.h"
 using namespace std;
 
-
 int main()
-{  
+{
     // Create a client object to handle user interactions and requests
     Client client;
     // Start the client's request-handling loop
